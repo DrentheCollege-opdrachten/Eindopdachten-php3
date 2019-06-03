7b7d94e5ad74e6449df369b728ec14afbea67c92
Michel Gerding <124099@student.drenthecollege.nl>
2019-06-03 10:59:09 +0200

    removed test

--- opdracht 2/php/classes/cell.php
+++ opdracht 2/php/classes/cell.php
@@ -9,7 +9,7 @@ class cell {
   }
 
   public function show () {
-    echo "<td class=\"cell $this->numb\"id= \"$this->id\" onclick=\"clicked('a')\"> ? </td>\n";
+    echo "<td class=\"cell $this->numb\"id= \"$this->id\" onclick=\"clicked($(this))\"> ? </td>\n";
   }
 
 }
