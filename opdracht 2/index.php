a01aae19622b81f9ada1a9fb8fcd23da3c22c52a
Michel Gerding <124099@student.drenthecollege.nl>
2019-06-03 10:46:21 +0200

    set the cells to a known place

--- opdracht 2/index.php
+++ opdracht 2/index.php
@@ -8,22 +8,47 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="./css/master.css">
     <title>eindopdracht 2 | php 3</title>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
   </head>
   <body>
     <div class="container">
       <table>
         <?php
-        try {
-          $screen = new Screen();
-          $screen->show();
-        } catch (\Exception $e) { echo $e;}
+        // try {
+        //   $screen = new Screen();
+        //   $screen->show();
+        // } catch (\Exception $e) { echo $e;}
 
         ?>
+        <tbody><tr id="row0">
+        <td class="cell 1" id="0" onclick="clicked($(this))"> ? </td>
+        <td class="cell 1" id="1" onclick="clicked($(this))"> ? </td>
+        <td class="cell 2" id="2" onclick="clicked($(this))"> ? </td>
+        <td class="cell 2" id="3" onclick="clicked($(this))"> ? </td>
+        </tr>
+        <tr id="row1">
+        <td class="cell 3" id="4" onclick="clicked($(this))"> ? </td>
+        <td class="cell 3" id="5" onclick="clicked($(this))"> ? </td>
+        <td class="cell 4" id="6" onclick="clicked($(this))"> ? </td>
+        <td class="cell 4" id="7" onclick="clicked($(this))"> ? </td>
+        </tr>
+        <tr id="row2">
+        <td class="cell 5" id="8" onclick="clicked($(this))"> ? </td>
+        <td class="cell 5" id="9" onclick="clicked($(this))"> ? </td>
+        <td class="cell 6" id="10" onclick="clicked($(this))"> ? </td>
+        <td class="cell 6" id="11" onclick="clicked($(this))"> ? </td>
+        </tr>
+        <tr id="row3">
+        <td class="cell 7" id="12" onclick="clicked($(this))"> ? </td>
+        <td class="cell 7" id="13" onclick="clicked($(this))"> ? </td>
+        <td class="cell 8" id="14" onclick="clicked($(this))"> ? </td>
+        <td class="cell 8" id="15" onclick="clicked($(this))"> ? </td>
+        </tr>
+              </tbody>
       </table>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="./js/app.js" charset="utf-8"></script>
   </body>
 </html>
