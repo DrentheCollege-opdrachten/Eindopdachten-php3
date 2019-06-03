601f87af5695812e3c04ce013db704a8a2fa60c8
Michel Gerding <124099@student.drenthecollege.nl>
2019-06-03 11:41:04 +0200

    added and styled a scoreboard

--- opdracht 2/index.php
+++ opdracht 2/index.php
@@ -13,6 +13,15 @@
   </head>
   <body>
     <div class="container">
+      <div class="playerscores">
+        <div id="player1">
+          <span>player 1: <span id="player1_score"> 0 </span><span> points </span></span>
+        </div>
+        <div id="player2">
+          <span>player 2: <span id="player2_score"> 0 </span><span> points </span></span>
+        </div>
+      </div>
+
       <table>
         <?php
         // try {
@@ -21,31 +30,32 @@
         // } catch (\Exception $e) { echo $e;}
 
         ?>
-        <tbody><tr id="row0">
-        <td class="cell 1" id="0" onclick="clicked($(this))"> ? </td>
-        <td class="cell 1" id="1" onclick="clicked($(this))"> ? </td>
-        <td class="cell 2" id="2" onclick="clicked($(this))"> ? </td>
-        <td class="cell 2" id="3" onclick="clicked($(this))"> ? </td>
-        </tr>
-        <tr id="row1">
-        <td class="cell 3" id="4" onclick="clicked($(this))"> ? </td>
-        <td class="cell 3" id="5" onclick="clicked($(this))"> ? </td>
-        <td class="cell 4" id="6" onclick="clicked($(this))"> ? </td>
-        <td class="cell 4" id="7" onclick="clicked($(this))"> ? </td>
-        </tr>
-        <tr id="row2">
-        <td class="cell 5" id="8" onclick="clicked($(this))"> ? </td>
-        <td class="cell 5" id="9" onclick="clicked($(this))"> ? </td>
-        <td class="cell 6" id="10" onclick="clicked($(this))"> ? </td>
-        <td class="cell 6" id="11" onclick="clicked($(this))"> ? </td>
-        </tr>
-        <tr id="row3">
-        <td class="cell 7" id="12" onclick="clicked($(this))"> ? </td>
-        <td class="cell 7" id="13" onclick="clicked($(this))"> ? </td>
-        <td class="cell 8" id="14" onclick="clicked($(this))"> ? </td>
-        <td class="cell 8" id="15" onclick="clicked($(this))"> ? </td>
-        </tr>
-              </tbody>
+          <tbody>
+            <tr id="row0">
+              <td class="cell 1" id="0" onclick="clicked($(this))"> ? </td>
+              <td class="cell 1" id="1" onclick="clicked($(this))"> ? </td>
+              <td class="cell 2" id="2" onclick="clicked($(this))"> ? </td>
+              <td class="cell 2" id="3" onclick="clicked($(this))"> ? </td>
+            </tr>
+            <tr id="row1">
+              <td class="cell 3" id="4" onclick="clicked($(this))"> ? </td>
+              <td class="cell 3" id="5" onclick="clicked($(this))"> ? </td>
+              <td class="cell 4" id="6" onclick="clicked($(this))"> ? </td>
+              <td class="cell 4" id="7" onclick="clicked($(this))"> ? </td>
+            </tr>
+            <tr id="row2">
+              <td class="cell 5" id="8" onclick="clicked($(this))"> ? </td>
+              <td class="cell 5" id="9" onclick="clicked($(this))"> ? </td>
+              <td class="cell 6" id="10" onclick="clicked($(this))"> ? </td>
+              <td class="cell 6" id="11" onclick="clicked($(this))"> ? </td>
+            </tr>
+            <tr id="row3">
+              <td class="cell 7" id="12" onclick="clicked($(this))"> ? </td>
+              <td class="cell 7" id="13" onclick="clicked($(this))"> ? </td>
+              <td class="cell 8" id="14" onclick="clicked($(this))"> ? </td>
+              <td class="cell 8" id="15" onclick="clicked($(this))"> ? </td>
+            </tr>
+        </tbody>
       </table>
     </div>
 
--- opdracht 2/js/app.js
+++ opdracht 2/js/app.js
@@ -1,4 +1,7 @@
 let cells = [null, null];
+const player1 = $('#player1_score');
+const player2 = $('#player2_score');
+let currentPlayer = 0; // to get the current player increase by 1
 //variables used for testing
 let delay = 1; // time delay in seconds
 
@@ -25,8 +28,21 @@ function checkCorrect () {
     } else {
       wrong();
     }
+    nextPlayer();
     hideNumber();
   }, delay * 1000);
+}
+
+function nextPlayer() {
+  if (currentPlayer) {
+    document.getElementById(`player${currentPlayer + 1}`).style.backgroundColor = "white";
+    currentPlayer = 0;
+    document.getElementById(`player${currentPlayer + 1}`).style.backgroundColor = "#29ffcd";
+  } else {
+    document.getElementById(`player${currentPlayer + 1}`).style.backgroundColor = "white";
+    currentPlayer = 1;
+    document.getElementById(`player${currentPlayer + 1}`).style.backgroundColor = "#29ffcd";
+  }
 }
 function wrong () {console.log('wrong');}
 function correct () {
@@ -39,7 +55,7 @@ function hideNumber () {
     cells[i].text('?');
   }
   cells[0] = cells[1] = null;
-  
+
 }
 
 function hidecells() {
